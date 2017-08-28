b323e29456cedaf54823b05ef31fb71466e1de1c
Clinton Pahl <clinton.p@getjobber.com>
2017-08-28 11:57:50 -0600

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,9 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      # Handle successful save
+      # Could have also just done: redirect_to @user
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to user_url(@user)
     else
       render 'new'
     end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,36 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid signup information" do
+    assert_no_difference 'User.count' do
+      post users_path params: { user: {
+          name: "",
+          email: "user@invalid",
+          password: "some password",
+          password_confirmation: "asdf"
+      }}
+
+      assert_template 'users/new'
+      assert_select 'div#error_explanation'
+      assert_select 'div.field_with_errors'
+      assert_select 'form[action="/signup"]'
+    end
+  end
+
+  test "valid signup informtion" do
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+      post users_path params: { user: {
+        name: "vald name",
+        email: "valid@valid.com",
+        password: "validpassword",
+        password_confirmation: "validpassword"
+      }}
+    end
+
+    follow_redirect!
+    assert_template 'users/show'
+    assert_select 'div.alert'
+  end
 end
